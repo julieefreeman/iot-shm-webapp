4c709dbd4db951902d9f898b698e3892fe02c9fe
Julie Freeman <julieefreeman@gmail.com>
2015-03-31 13:08:10 -0400

    adding healhty/unhealthy panel

--- webapp/iotshm_dashboard/views.py
+++ webapp/iotshm_dashboard/views.py
@@ -171,18 +171,18 @@ def user_login(request):
 
 
 def real_time_ajax_x(request,building_num):
-    curr_building_sensors = SensorRDS.objects.using('data').filter(building_id=building_num)
-    curr_building_sensors = [str(s.id) for s in curr_building_sensors]
-    ts = HealthScore.objects.using('data').filter(timestamp__lt=timezone.now()).order_by('timestamp')[0].timestamp
-    magnitudes = Magnitude.objects.using('data').filter(timestamp=make_aware(ts,utc))
+    # curr_building_sensors = SensorRDS.objects.using('data').filter(building_id=building_num)
+    # curr_building_sensors = [str(s.id) for s in curr_building_sensors]
+    # ts = HealthScore.objects.using('data').filter(timestamp__lt=timezone.now()).order_by('timestamp')[0].timestamp
+    # magnitudes = Magnitude.objects.using('data').filter(timestamp=make_aware(ts,utc))
     json_response = {}
-    for m in magnitudes:
-        if m.sensor_id in curr_building_sensors:
-            if not m.sensor_id in json_response:
-                json_response[m.sensor_id] = {"x_data": [], "y_data": [], "z_data": []}
-            json_response[m.sensor_id]["x_data"].append({"frequency": m.frequency,"x_magnitude": m.x_magnitude})
-            json_response[m.sensor_id]["y_data"].append({"frequency": m.frequency,"y_magnitude": m.y_magnitude})
-            json_response[m.sensor_id]["z_data"].append({"frequency": m.frequency,"z_magnitude": m.z_magnitude})
+    # for m in magnitudes:
+    #     if m.sensor_id in curr_building_sensors:
+    #         if not m.sensor_id in json_response:
+    #             json_response[m.sensor_id] = {"x_data": [], "y_data": [], "z_data": []}
+    #         json_response[m.sensor_id]["x_data"].append({"frequency": m.frequency,"x_magnitude": m.x_magnitude})
+    #         json_response[m.sensor_id]["y_data"].append({"frequency": m.frequency,"y_magnitude": m.y_magnitude})
+    #         json_response[m.sensor_id]["z_data"].append({"frequency": m.frequency,"z_magnitude": m.z_magnitude})
 
     # json_response ={SensorRDS.objects.using('data').filter(building_id=building_num)[0].id: {
     #           "data": [
@@ -200,18 +200,18 @@ def real_time_ajax_x(request,building_num):
         content_type='application/json')
 
 def real_time_ajax_y(request,building_num):
-    curr_building_sensors = SensorRDS.objects.using('data').filter(building_id=building_num)
-    curr_building_sensors = [str(s.id) for s in curr_building_sensors]
-    ts = HealthScore.objects.using('data').filter(timestamp__lt=timezone.now()).order_by('timestamp')[0].timestamp
-    magnitudes = Magnitude.objects.using('data').filter(timestamp=make_aware(ts,utc))
+    # curr_building_sensors = SensorRDS.objects.using('data').filter(building_id=building_num)
+    # curr_building_sensors = [str(s.id) for s in curr_building_sensors]
+    # ts = HealthScore.objects.using('data').filter(timestamp__lt=timezone.now()).order_by('timestamp')[0].timestamp
+    # magnitudes = Magnitude.objects.using('data').filter(timestamp=make_aware(ts,utc))
     json_response = {}
-    for m in magnitudes:
-        if m.sensor_id in curr_building_sensors:
-            if not m.sensor_id in json_response:
-                json_response[m.sensor_id] = {"x_data": [], "y_data": [], "z_data": []}
-            json_response[m.sensor_id]["x_data"].append({"frequency": m.frequency,"x_magnitude": m.x_magnitude})
-            json_response[m.sensor_id]["y_data"].append({"frequency": m.frequency,"y_magnitude": m.y_magnitude})
-            json_response[m.sensor_id]["z_data"].append({"frequency": m.frequency,"z_magnitude": m.z_magnitude})
+    # for m in magnitudes:
+    #     if m.sensor_id in curr_building_sensors:
+    #         if not m.sensor_id in json_response:
+    #             json_response[m.sensor_id] = {"x_data": [], "y_data": [], "z_data": []}
+    #         json_response[m.sensor_id]["x_data"].append({"frequency": m.frequency,"x_magnitude": m.x_magnitude})
+    #         json_response[m.sensor_id]["y_data"].append({"frequency": m.frequency,"y_magnitude": m.y_magnitude})
+    #         json_response[m.sensor_id]["z_data"].append({"frequency": m.frequency,"z_magnitude": m.z_magnitude})
 
     # json_response ={SensorRDS.objects.using('data').filter(building_id=building_num)[0].id: {
     #           "data": [
@@ -229,18 +229,18 @@ def real_time_ajax_y(request,building_num):
         content_type='application/json')
 
 def real_time_ajax_z(request,building_num):
-    curr_building_sensors = SensorRDS.objects.using('data').filter(building_id=building_num)
-    curr_building_sensors = [str(s.id) for s in curr_building_sensors]
-    ts = HealthScore.objects.using('data').filter(timestamp__lt=timezone.now()).order_by('timestamp')[0].timestamp
-    magnitudes = Magnitude.objects.using('data').filter(timestamp=make_aware(ts,utc))
+    # curr_building_sensors = SensorRDS.objects.using('data').filter(building_id=building_num)
+    # curr_building_sensors = [str(s.id) for s in curr_building_sensors]
+    # ts = HealthScore.objects.using('data').filter(timestamp__lt=timezone.now()).order_by('timestamp')[0].timestamp
+    # magnitudes = Magnitude.objects.using('data').filter(timestamp=make_aware(ts,utc))
     json_response = {}
-    for m in magnitudes:
-        if m.sensor_id in curr_building_sensors:
-            if not m.sensor_id in json_response:
-                json_response[m.sensor_id] = {"x_data": [], "y_data": [], "z_data": []}
-            json_response[m.sensor_id]["x_data"].append({"frequency": m.frequency,"x_magnitude": m.x_magnitude})
-            json_response[m.sensor_id]["y_data"].append({"frequency": m.frequency,"y_magnitude": m.y_magnitude})
-            json_response[m.sensor_id]["z_data"].append({"frequency": m.frequency,"z_magnitude": m.z_magnitude})
+    # for m in magnitudes:
+    #     if m.sensor_id in curr_building_sensors:
+    #         if not m.sensor_id in json_response:
+    #             json_response[m.sensor_id] = {"x_data": [], "y_data": [], "z_data": []}
+    #         json_response[m.sensor_id]["x_data"].append({"frequency": m.frequency,"x_magnitude": m.x_magnitude})
+    #         json_response[m.sensor_id]["y_data"].append({"frequency": m.frequency,"y_magnitude": m.y_magnitude})
+    #         json_response[m.sensor_id]["z_data"].append({"frequency": m.frequency,"z_magnitude": m.z_magnitude})
 
     # json_response ={SensorRDS.objects.using('data').filter(building_id=building_num)[0].id: {
     #           "data": [
